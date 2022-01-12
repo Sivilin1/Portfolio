05b7f94443fdeeae820a1752a060c4d6a072b166
Sivilin1 <89265738+Sivilin1@users.noreply.github.com>
2022-01-12 19:09:00 +0000

    Add files via upload
    SQL requests

---
+++ OR.js
@@ -0,0 +1,11 @@
+describe("log in", function() {
+    it("Sing in", function() {
+        cy.visit("https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all")
+        cy.wait(1000)
+        cy.get('[title="Click to see the content of the Customers table"]').click()
+        cy.wait(3000)
+        cy.get('.CodeMirror pre').type('{enter}WHERE City ="Berlin" OR Country= "Germany"')
+        cy.wait(1000)
+        cy.get('.ws-btn').click();
+    })
+})
